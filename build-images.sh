c0f96084aa084a3b9c2f51b82c2d6c6a2fef5036
Martin Bhuong <martin@genius.ke>
2024-06-26 22:00:58 +0200

    update image tag to main

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/mongo:7.0.9 docker.io/amolshejole/opensign:staging docker.io/amolshejole/opensignserver:staging" \
+    --label="org.nethserver.images=docker.io/mongo:7.0.9 docker.io/amolshejole/opensign:main docker.io/amolshejole/opensignserver:main" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
